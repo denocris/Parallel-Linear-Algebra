9b444330bf7bb2fe2b7a655e7006088602d75774
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-03-27 16:51:35 +0200

    adding fortran binding

---
+++ D1/test.f
@@ -0,0 +1,20 @@
+      program test
+      use iso_c_binding
+      implicit none
+      
+      integer(c_int) n
+      real(c_double),allocatable,dimension(:) :: mat
+      real(c_double):: cond_numb
+      
+      n=2
+      allocate(mat(n*n))
+      cond_numb=0.001
+      
+      call fill_defpos_symm_matrix(mat,cond_numb,n)
+      
+      write(*,*) mat
+      
+      stop
+      end
+
+      
