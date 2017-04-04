553cf351be07da9ecea5fccc749f501be0de9f28
denocris <denobili16@gmail.com>
2017-04-04 10:13:29 +0200

    mod main bench

--- D3/grad_laplace_MPI/src_bench/script_mpi_strongsc.sh
+++ D3/grad_laplace_MPI/src_bench/script_mpi_strongsc.sh
@@ -8,9 +8,7 @@ module load openmpi
 
 #for ((size=100;size<=1000;size+=100))
 size=500000
+for i in  1 2 4 8 16
 do
-	for i in  1 2 4 8 16
-	do
-		mpirun -np $i ./main_benchmark.x $size  #>> $outfile
-	done
+	mpirun -np $i ./main_benchmark.x $size  #>> $outfile
 done
